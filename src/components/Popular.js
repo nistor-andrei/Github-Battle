183a62cedc97e87826e3296c1c0174f3b58dc4ae
Nistor Andrei <andreimn97@yahoo.com>
2021-09-29 23:07:47 +0300

    nav updated

--- src/components/Popular.js
+++ src/components/Popular.js
@@ -1,13 +1,19 @@
-import { useState } from "react";
-import "../index.css";
-import { LanguageNav } from "./LanguageNav";
+import { useState } from 'react';
+import '../index.css';
+import { LanguageNav } from './LanguageNav';
+import { PropTypes } from 'prop-types';
 
-export function Popular(props) {
-  const [languageCurrent, setLanguage] = useState("All");
+export function Popular() {
+  const [languageCurrent, setLanguage] = useState('All');
 
   function updateLanguage(selectedLanguage) {
     setLanguage(selectedLanguage);
   }
+
+  LanguageNav.propTypes = {
+    selected: PropTypes.string.isRequired,
+    updatedLanguage: PropTypes.func.isRequired,
+  };
 
   return (
     <>
